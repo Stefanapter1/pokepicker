3c01659d0e68b1cfc76bb3a49c2d5c32bb8a2a16
Stefan Apter <apter.stefan@gmail.com>
2023-11-13 11:43:53 +0700

    Wrong Code- Learning to filter
    Here i tried to create a filter for each pokemon that is defined as 'Fire' type.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Pokedex from 'pokedex-promise-v2';
+import axios from "axios";
 const P = new Pokedex();
 const app = express();
 const port = 4000;
@@ -18,9 +19,33 @@ app.get('/', async (req, res) => {
 // this generated team memeber is then pushed into the array after one another - giving a team of six pokemeon.
     team.push(species);
   }
-
   res.render('index.ejs', { team });
 });
+
+app.get('/firepokemon', async (req,res) => {
+  const team = [];
+  const fetchPokemonData = async () => {
+  try {
+     const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1017');
+     const fireTypePokemon = response.data.results.filter(pokemon => pokemon.type.includes('Fire'));
+     console.log(fireTypePokemon);
+  } catch (error) {
+     console.error(error);
+  }
+  if (fireTypePokemon === true) {
+    for (let i = 0; i < 6; i++) {
+    const fireData = await fetchPokemonData();
+    (
+      Math.round(Math.random() * 1017)
+   );
+   team.push(fireData);
+  }
+  } else {
+    fetchPokemonData();
+  }
+ };
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
